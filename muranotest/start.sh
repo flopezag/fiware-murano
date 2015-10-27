f5b7291d06ab7a6d5715ebf80e7444bbd79f3dfe
Henar Muñoz Frutos <henar.munozfrutos@telefonica.com>
2015-10-27 10:34:46 +0100

    adding files to muranotest

--- muranotest/start.sh
+++ muranotest/start.sh
@@ -5,4 +5,6 @@ echo $ip
 echo "$ip murano.lab.fiware.org" >> /etc/hosts
 while ! nc -z murano 8082; do sleep 8; done
 curl http://$ip:8082
+git fetch https://review.openstack.org/openstack/murano refs/changes/$REVISION && git checkout FETCH_HEAD
+sed -i -e "s/XXX/${PASSWORD}/" /opt/murano/murano/tests/functional/engine/config.conf
 sed -i -e "s/XXX/${PASSWORD}/" /etc/tempest/tempest.conf
