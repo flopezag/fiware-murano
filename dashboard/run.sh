0efb11cdeac8cf324c6b048d80f3c6c5d7e14312
Jesus Perez <jesus.perezgonzalez@telefonica.com>
2015-10-29 14:26:15 +0100

    improve run.sh script: stop and clean previous installation

--- dashboard/run.sh
+++ dashboard/run.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+
+
+MURANO=`docker ps -a -q --filter="name=murano"`
+echo $MURANO
+if [ "$MURANO" != "" ]
+then
+    echo "stop murano"
+    docker stop $(docker ps -a -q)
+    docker rm $(docker ps -a -q)
+    docker rmi murano-dashboard
+    docker rmi fiware-murano
+fi
 cd ../fiwaremurano
 docker build -t fiware-murano .
 cd ../dashboard
