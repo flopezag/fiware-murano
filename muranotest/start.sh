503a094a32eed07436d665de6123d01397569a2c
Henar Muñoz Frutos <henar.munozfrutos@telefonica.com>
2015-12-15 16:07:04 +0100

    Including centos and debian for testing

--- muranotest/start.sh
+++ muranotest/start.sh
@@ -10,8 +10,16 @@ pip install -r requirements.txt
 pip install -r test-requirements.txt
 python setup.py install
 sed -i -e "s/XXX/${PASSWORD}/" /opt/murano/murano/tests/functional/engine/config.conf
+sed -i -e "s/XXX/${PASSWORD}/" /opt/murano/murano/tests/functional/engine/config2.conf
+sed -i -e "s/XXX/${PASSWORD}/" /opt/murano/murano/tests/functional/engine/config3.conf
 sed -i -e "s/XXX/${PASSWORD}/" /etc/tempest/tempest.conf
 while ! nc -z murano 8082; do sleep 8; done
 export TEMPEST_CONFIG_DIR=/etc/tempest/
 export TEMPEST_CONFIG=tempest.conf
 cat /etc/tempest/tempest.conf
+nosetests --with-xunit --xunit-file /opt/test2.xml murano.tests.functional.engine
+cp /opt/murano/murano/tests/functional/engine/config2.conf /opt/murano/murano/tests/functional/engine/config.conf
+nosetests --with-xunit --xunit-file /opt/test3.xml murano.tests.functional.engine
+cp /opt/murano/murano/tests/functional/engine/config3.conf /opt/murano/murano/tests/functional/engine/config.conf
+nosetests --with-xunit --xunit-file /opt/test4.xml murano.tests.functional.engine
+
