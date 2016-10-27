117633d56740cf09b55558982e75e6172bf96417
visitor <henar.munozfrutos@telefonica.com>
2016-10-27 16:47:54 +0200

    update run script

--- docker/run.sh
+++ docker/run.sh
@@ -13,4 +13,7 @@ then
 fi
 docker build -t fiware-murano .
 export PASSWORD=$1
+export RABBIT_HOST=$2
+export RABBIT_LOGIN=$3
+export RABBIT_PASSWORD=$4
 docker-compose -f docker-compose-nets.yml  up -d
