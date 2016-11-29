f9835da7a67ebbf7f9980a3a321f260b92cdebf6
visitor <henar.munozfrutos@telefonica.com>
2016-11-29 15:38:57 +0100

    Update documentation and dockerfile for installing murano as a service

---
+++ docker/muranoservice/start.sh
@@ -0,0 +1,11 @@
+apt-get install -y netcat curl host syslinux-utils
+while ! nc -z mysql 3306; do sleep 8; done
+sed -i -e "s/RABBIT_HOST/${RABBIT_HOST}/" /etc/murano/class-configs/io.murano.Environment.yaml 
+sed -i -e "s/RABBIT_PASSWORD/${RABBIT_PASSWORD}/" /etc/murano/class-configs/io.murano.Environment.yaml
+sed -i -e "s/RABBIT_LOGIN/${RABBIT_LOGIN}/" /etc/murano/class-configs/io.murano.Environment.yaml
+sed -i -e "s/XXX/${PASSWORD}/" /etc/murano/murano.conf
+murano-db-manage upgrade
+service murano-api restart
+service murano-engine stop
+service murano-engine start
+sleep 120000
